c96cf5878ff57bffd22dd3b29b3c87b1d30c5d57
Paolina Petkova <paolina.petkova@ocado.com>
2022-05-30 12:32:04 +0300

    Support multiple dashboard creation.

--- dashboard-creation/create_dashboards.py
+++ dashboard-creation/create_dashboards.py
@@ -19,35 +19,37 @@ def check_for_supported_measurements(measurements):
             help_message()
             exit()
 
-def get_dashboard_path():
-    return path.join('..', 'provisioning', 'dashboards', 'Measurements.json')
+def get_dashboard_path(dashboard):
+    return path.join('..', 'provisioning', 'dashboards', dashboard+".json")
 
 def create_dashboard():
     dashboard_config = load_temp.load_dashboard_visualizations()
-    final_dashboard = load_temp.load_template(load_temp.MEASUREMENTS_DASHBOARD)
+    for dash in range(len(dashboard_config)):
+        final_dashboard = load_temp.load_template(load_temp.MEASUREMENTS_DASHBOARD)
+        final_dashboard["title"] = dashboard_config[dash]["name"]
+        final_dashboard["id"] = dash + 1
+        for visual in dashboard_config[dash]["visualizations"]:
+            print("visual ", visual)
+            if visual["type"] == "bar_gauge":
+                final_dashboard = dashboard.add_bar_gauge(visual, final_dashboard)
+            elif visual["type"] == "gauge":
+                final_dashboard = dashboard.add_gauge(visual, final_dashboard)
+            elif visual["type"] == "graph":
+                final_dashboard = dashboard.add_graph(visual, final_dashboard)
+            elif visual["type"] == "heat_map":
+                final_dashboard = dashboard.add_heat_map(visual, final_dashboard)
+            elif visual["type"] == "pie_chart":
+                final_dashboard = dashboard.add_pie_chart(visual, final_dashboard)
+            elif visual["type"] == "stat":
+                final_dashboard = dashboard.add_stat(visual, final_dashboard)
+            elif visual["type"] == "table":
+                final_dashboard = dashboard.add_table(visual, final_dashboard)
+            elif visual["type"] == "time_series":
+                final_dashboard = dashboard.add_time_series(visual, final_dashboard)
+        with open(get_dashboard_path(dashboard_config[dash]["name"]), 'w') as f:
+            json.dump(final_dashboard, f)
+        print("Dashboard is successfully created.")
 
-    for visual in dashboard_config["visualizations"]:
-        print(visual["measurements"])
-        if visual["type"] == "bar_gauge":
-            final_dashboard = dashboard.add_bar_gauge(visual, final_dashboard)
-        elif visual["type"] == "gauge":
-            final_dashboard = dashboard.add_gauge(visual, final_dashboard)
-        elif visual["type"] == "graph":
-            final_dashboard = dashboard.add_graph(visual, final_dashboard)
-        elif visual["type"] == "heat_map":
-            final_dashboard = dashboard.add_heat_map(visual, final_dashboard)
-        elif visual["type"] == "pie_chart":
-            final_dashboard = dashboard.add_pie_chart(visual, final_dashboard)
-        elif visual["type"] == "stat":
-            final_dashboard = dashboard.add_stat(visual, final_dashboard)
-        elif visual["type"] == "table":
-            final_dashboard = dashboard.add_table(visual, final_dashboard)
-        elif visual["type"] == "time_series":
-            final_dashboard = dashboard.add_time_series(visual, final_dashboard)
-
-    with open(get_dashboard_path(), 'w') as f:
-        json.dump(final_dashboard, f)
-    print("Dashboard is successfully created.")
     return True
 
 if __name__ == '__main__':
